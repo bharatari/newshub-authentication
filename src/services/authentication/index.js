db7b882bb8a26d2973c0efb1d97b62382f32e280
Bharat Arimilli <bbarimilli@outlook.com>
2016-07-13 15:33:00 -0500

    Hardcode non-sensitive authentication configuration options

--- src/services/authentication/index.js
+++ src/services/authentication/index.js
@@ -6,7 +6,22 @@ const authentication = require('feathers-authentication');
 module.exports = function() {
   const app = this;
 
-  let config = app.get('auth');
-  
+  const auth = app.get('auth');
+
+  let config = {
+    userEndpoint: "/api/user",
+    localEndpoint: "/api/login",
+    successRedirect: false,
+    failureRedirect: false,
+    shouldSetupSuccessRoute: false,
+    shouldSetupFailureRoute: false,
+    idField: "id",
+    local: {
+      usernameField: "username",
+    },
+  };
+
+  Object.assign(config, auth);
+
   app.configure(authentication(config));
 };
