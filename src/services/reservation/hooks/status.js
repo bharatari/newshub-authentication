f6704208c4d6402d518bfaf3593532e4299bc758
Bharat Arimilli <bbarimilli@outlook.com>
2016-07-15 14:12:03 -0500

    Update reservation service status hook with emailing and rejection support

--- src/services/reservation/hooks/status.js
+++ src/services/reservation/hooks/status.js
@@ -2,29 +2,49 @@
 
 const user = require('../../user/utils');
 const errors = require('feathers-errors');
+const email = require('../../../utils/email');
 
 module.exports = function (options) {
   return function (hook) {
     const models = hook.app.get('sequelize').models;
-    const { approved, checkedOut, checkedIn } = hook.data;
+    const { approved, checkedOut, checkedIn, adminNotes, disabled } = hook.data;
 
-    if (approved || checkedOut || checkedIn) {
+    if (approved || checkedOut || checkedIn || adminNotes || disabled) {
       return models.reservation.findOne({
         where: {
           id: hook.id,
         },
+        include: [{
+          model: models.user,
+        }],
       }).then(function (reservation) {
         if (approved && !reservation.approved) {
           if (user.isAdmin(hook.params.user)) {
             hook.data.approvedById = hook.params.user.id;
+
+            return email.sendEmail(hook.app, reservation.user.email, null, 'approved', 'USER_RESERVATION_RESPONSE')
+              .then(function (response) {
+                return hook;
+              }).catch(function (err) {
+                // Don't throw error just because email didn't send
+                return hook;
+              });
           } else {
             throw new errors.NotAuthenticated('Must be an admin to update reservation status.');
-          }          
+          }
         }
 
         if (checkedOut && !reservation.checkedOut) {
           if (user.isAdmin(hook.params.user)) {
             hook.data.checkedOutById = hook.params.user.id;
+
+            return email.sendEmail(hook.app, reservation.user.email, null, 'checked out', 'USER_RESERVATION_RESPONSE')
+              .then(function (response) {
+                return hook;
+              }).catch(function (err) {
+                // Don't throw error just because email didn't send
+                return hook;
+              });
           } else {
             throw new errors.NotAuthenticated('Must be an admin to update reservation status.');
           }
@@ -33,9 +53,47 @@ module.exports = function (options) {
         if (checkedIn && !reservation.checkedIn) {
           if (user.isAdmin(hook.params.user)) {
             hook.data.checkedInById = hook.params.user.id;
+
+            return email.sendEmail(hook.app, reservation.user.email, null, 'checked in', 'USER_RESERVATION_RESPONSE')
+              .then(function (response) {
+                return hook;
+              }).catch(function (err) {
+                // Don't throw error just because email didn't send
+                return hook;
+              });
           } else {
             throw new errors.NotAuthenticated('Must be an admin to update reservation status.');
           }          
+        }
+
+        if (adminNotes && (reservation.adminNotes !== adminNotes)) {
+          if (user.isAdmin(hook.params.user)) {
+            return email.sendEmail(hook.app, reservation.user.email, null, 'added notes to', 'USER_RESERVATION_RESPONSE')
+              .then(function (response) {
+                return hook;
+              }).catch(function (err) {
+                // Don't throw error just because email didn't send
+                return hook;
+              });
+          } else {
+            throw new errors.NotAuthenticated('Must be an admin to update reservation admin notes.');
+          }
+        }
+
+        if (disabled && !reservation.disabled) {
+          if (user.isAdmin(hook.params.user)) {
+            hook.data.disabledById = hook.params.user.id;
+
+            return email.sendEmail(hook.app, reservation.user.email, null, 'rejected', 'USER_RESERVATION_RESPONSE')
+              .then(function (response) {
+                return hook;
+              }).catch(function (err) {
+                // Don't throw error just because email didn't send
+                return hook;
+              });
+          } else {
+            throw new errors.NotAuthenticated('Must be an admin to update reservation status.');
+          }
         }
 
         return hook;
