71d3e7458dc5902d05bad200ed10d648ca8fef4f
Bharat Arimilli <bbarimilli@outlook.com>
2016-10-06 12:44:00 -0500

    Only run sanitize hook for external calls to avoid interfering with login

--- src/services/user/hooks/sanitize.js
+++ src/services/user/hooks/sanitize.js
@@ -4,16 +4,18 @@ const userUtils = require('../../user/utils');
 
 module.exports = function (options) {
   return function (hook) {
-    if (hook.type === 'before') {
-      if (!userUtils.isAdmin(hook.params.user)) {
-        hook.params.sequelize = {
-          where: {
-            disabled: false,
-          },
-        };
-      } else {
-        hook.params.sequelize = {};
-      }      
+    if (hook.params.provider) {
+      if (hook.type === 'before') {
+        if (!userUtils.isAdmin(hook.params.user)) {
+          hook.params.sequelize = {
+            where: {
+              disabled: false,
+            },
+          };
+        } else {
+          hook.params.sequelize = {};
+        }      
+      }
     }
 
     return hook;    
--- src/services/user/utils.js
+++ src/services/user/utils.js
@@ -5,33 +5,37 @@ const _ = require('lodash');
 
 module.exports = {
   isAdmin(user) {
-    if (user.dataValues) {
-      if (user.dataValues.roles) {
-        if (_.isString(user.dataValues.roles)) {
-          return user.dataValues.roles.includes('admin') || user.dataValues.roles.includes('master');
+    if (user) {
+      if (user.dataValues) {
+        if (user.dataValues.roles) {
+          if (_.isString(user.dataValues.roles)) {
+            return user.dataValues.roles.includes('admin') || user.dataValues.roles.includes('master');
+          }
+        }
+      } else {
+        if (user.roles) {
+          if (_.isString(user.roles)) {
+            return user.roles.includes('admin') || user.roles.includes('master');
+          }
         }
       }
-    } else {
-      if (user.roles) {
-        if (_.isString(user.roles)) {
-          return user.roles.includes('admin') || user.roles.includes('master');
-        }
-      }
-    }
+    }    
     
     return false;
   },
   isMaster(user) {
-    if (user.dataValues) {
-      if (user.dataValues.roles) {
-        if (_.isString(user.dataValues.roles)) {
-          return user.dataValues.roles.includes('master');
+    if (user) {
+      if (user.dataValues) {
+        if (user.dataValues.roles) {
+          if (_.isString(user.dataValues.roles)) {
+            return user.dataValues.roles.includes('master');
+          }
         }
-      }
-    } else {
-      if (user.roles) {
-        if (_.isString(user.roles)) {
-          return user.roles.includes('master');
+      } else {
+        if (user.roles) {
+          if (_.isString(user.roles)) {
+            return user.roles.includes('master');
+          }
         }
       }
     }
