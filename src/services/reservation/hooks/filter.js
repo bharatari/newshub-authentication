940fdf7aecd9055b6d2a91c115c1e7383e467bda
Bharat Arimilli <bbarimilli@outlook.com>
2017-02-10 22:20:43 -0600

    Merge where query params when filtering reservations by date

--- src/services/reservation/hooks/filter.js
+++ src/services/reservation/hooks/filter.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const utils = require('../utils');
+const _ = require('lodash');
+const query = require('../../../utils/query');
 
 module.exports = function (options) {
   return function (hook) {
@@ -8,17 +10,21 @@ module.exports = function (options) {
 
     if (hook.type === 'before') {
       if (hook.params.query.startDate && hook.params.query.endDate) {
-        hook.params.sequelize = {
-          where: utils.overlaps(hook.params.query.startDate, hook.params.query.endDate),
-        };
+        const { startDate, endDate } = hook.params.query;
 
         delete hook.params.query.startDate;
         delete hook.params.query.endDate;
+
+        const where = Object.assign(query.getWhere(hook.params.query), utils.overlaps(startDate, endDate));
+
+        hook.params.sequelize = {
+          where,
+        };
       } else {
         hook.params.sequelize = {};
-      }      
+      }
     }
 
-    return hook;    
+    return hook;
   };
 };
