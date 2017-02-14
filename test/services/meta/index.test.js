aa989e7c9d3286993c67cfbbc53edeb481390b81
Bharat Arimilli <bbarimilli@outlook.com>
2017-02-13 21:57:09 -0600

    Fix meta service test

--- test/services/meta/index.test.js
+++ test/services/meta/index.test.js
@@ -5,6 +5,6 @@ const app = require('../../../src/app');
 
 describe('meta service', function() {
   it('registered the meta service', () => {
-    assert.ok(app.service('meta'));
+    assert.ok(app.service('/api/meta'));
   });
 });
