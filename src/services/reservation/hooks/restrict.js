4f4586a137bddbb0611bb48ef01a721b5a97ba61
Bharat Arimilli <bbarimilli@outlook.com>
2017-04-29 13:27:20 -0500

    Remove special requests from status on creation check

--- src/services/reservation/hooks/restrict.js
+++ src/services/reservation/hooks/restrict.js
@@ -4,9 +4,9 @@ const errors = require('feathers-errors');
 
 module.exports = function (options) {
   return function (hook) {
-    const { approved, checkedOut, checkedIn, adminNotes, disabled, specialRequests } = hook.data;
+    const { approved, checkedOut, checkedIn, adminNotes, disabled } = hook.data;
 
-    if (approved || checkedOut || checkedIn || adminNotes || disabled || specialRequests) {
+    if (approved || checkedOut || checkedIn || adminNotes || disabled) {
       throw new errors.BadRequest('Cannot set reservation status on creation');
     }
 
