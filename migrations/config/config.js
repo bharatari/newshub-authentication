b7cf4a815c1b6ead91514ea6e210a7ee1072097e
Bharat Arimilli <bbarimilli@outlook.com>
2017-05-04 10:21:57 -0500

    Update sequelize cli config

--- migrations/config/config.js
+++ migrations/config/config.js
@@ -2,11 +2,11 @@ const env = process.env.NODE_ENV || 'development';
 
 module.exports = {
   [env]: {
-    username: "postgres",
-    password: "postgres",
-    database: "newshub_server_development",
     url: process.env.DATABASE_URL,
     dialect: 'postgres',
-    migrationStorageTableName: '_migrations'
+    migrationStorageTableName: '_migrations',
+    dialectOptions: {
+      ssl: (env === 'development') ? false : true,
+    },
   }
 };
