1b45f04c7a322a19cbe53099ab81ecb6afdfc6ab
Bharat Arimilli <bbarimilli@outlook.com>
2017-05-04 04:34:26 -0500

    Check status ID fields on reservations on creation

--- src/services/reservation/hooks/restrict.js
+++ src/services/reservation/hooks/restrict.js
@@ -4,9 +4,18 @@ const errors = require('feathers-errors');
 
 module.exports = function (options) {
   return function (hook) {
-    const { approved, checkedOut, checkedIn, adminNotes, disabled } = hook.data;
+    const {
+      approved,
+      checkedOut,
+      checkedIn,
+      adminNotes,
+      disabled,
+      approvedById,
+      checkedOutById,
+      checkedInById
+    } = hook.data;
 
-    if (approved || checkedOut || checkedIn || adminNotes || disabled) {
+    if (approved || checkedOut || checkedIn || adminNotes || disabled || approvedById || checkedOutById || checkedInById) {
       throw new errors.BadRequest('Cannot set reservation status on creation');
     }
 
