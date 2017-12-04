78b9086ccae155d9f99dd404c1e4cbcb525929c8
Bharat Arimilli <bbarimilli@outlook.com>
2017-12-03 20:45:07 -0600

    Restructure app config

--- src/app.js
+++ src/app.js
@@ -5,12 +5,18 @@ const express = require('@feathersjs/express');
 const favicon = require('serve-favicon');
 const compress = require('compression');
 const cors = require('cors');
+const helmet = require('helmet');
+const logger = require('winston');
 const feathers = require('@feathersjs/feathers');
 const configuration = require('@feathersjs/configuration');
 const rest = require('@feathersjs/express/rest');
-const bodyParser = require('body-parser');
+const authentication = require('@feathersjs/authentication');
+const jwt = require('@feathersjs/authentication-jwt');
+const local = require('@feathersjs/authentication-local');
 const socketio = require('@feathersjs/socketio');
+
 const middleware = require('./middleware');
+const channels = require('./channels');
 const services = require('./services');
 const appHooks = require('./app.hooks');
 const skipper = require('skipper');
@@ -18,22 +24,54 @@ const redis = require('./utils/redis');
 
 const app = express(feathers());
 
-app.configure(configuration(path.join(__dirname, '..')));
+app.configure(configuration());
 
-app.use(compress())
-  .options('*', cors())
-  .use(cors())
-  .use(favicon(path.join(app.get('public'), 'favicon.ico')))
-  .use('/', express.static(app.get('public')))
-  .use('/app/*', express.static(app.get('public')))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
-  .use(skipper())
-  .configure(rest())
-  .configure(socketio())
-  .configure(services)
-  .configure(middleware)
-  .configure(redis)
-  .hooks(appHooks);
+app.use(cors());
+app.use(helmet());
+app.use(compress());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(favicon(path.join(app.get('public'), 'favicon.ico')));
+
+// Host the public folder
+app.use('/', express.static(app.get('public')));
+app.use('/app/*', express.static(app.get('public')));
+
+app.use(skipper());
+
+const authConfig = {
+  ...app.get('auth'),
+  path: '/api/login',
+  entity: 'user',
+  service: '/api/user',
+  local: {
+    usernameField: 'username',
+    service: '/api/user',
+  },
+};
+
+app.configure(authentication(authConfig))
+   .configure(local())
+   .configure(jwt());
+
+// Set up Plugins and providers
+app.configure(express.rest());
+app.configure(socketio());
+
+// Configure other middleware (see `middleware/index.js`)
+app.configure(middleware);
+// Set up our services (see `services/index.js`)
+app.configure(services);
+// Set up event channels (see channels.js)
+app.configure(channels);
+
+app.configure(redis);
+
+// Configure a middleware for 404s and the error handler
+app.use(express.notFound());
+app.use(express.errorHandler({ logger }));
+
+app.hooks(appHooks);
 
 module.exports = app;
--- src/services/authentication/index.js
+++ src/services/authentication/index.js
@@ -1,30 +1,9 @@
 'use strict';
 
-const authentication = require('@feathersjs/authentication');
-const jwt = require('@feathersjs/authentication-jwt');
-const local = require('@feathersjs/authentication-local');
 const hooks = require('./hooks');
 
 module.exports = function () {
   const app = this;
-
-  const auth = app.get('auth');
-
-  const config = {
-    path: '/api/login',
-    entity: 'user',
-    service: '/api/user',
-    local: {
-      usernameField: 'username',
-      service: '/api/user',
-    },
-  };
-
-  Object.assign(config, auth);
-
-  app.configure(authentication(config))
-     .configure(jwt())
-     .configure(local());
 
   // Get our initialize service to that we can bind hooks
   const service = app.service('/api/login');
