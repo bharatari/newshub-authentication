f9bbf1f351815049cc1eabb5451fc1df71a3658b
Bharat Arimilli <bbarimilli@outlook.com>
2017-12-04 13:05:59 -0600

    Remove old middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,15 +1,7 @@
 'use strict';
 
-const handler = require('@feathersjs/express/errors');
-const notFound = require('./not-found-handler');
-const logger = require('./logger');
-
 module.exports = function (app) {
   // Add your custom middleware here. Remember, that
   // just like Express the order matters, so error
   // handling middleware should go last.
-
-  app.use(notFound());
-  app.use(logger(app));
-  app.use(handler());
 };
