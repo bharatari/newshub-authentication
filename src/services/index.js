8cf5810bcc01ac5583957f05600d4949b8032874
Bharat Arimilli <bbarimilli@outlook.com>
2018-05-17 16:27:51 -0500

    Remove room and building services (move to newshub-room-reservations)

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,7 @@
 'use strict';
 
 const organization = require('./organization');
-const building = require('./building');
 const roomReservation = require('./roomReservation');
-const room = require('./room');
 const resetPassword = require('./resetPassword');
 const meta = require('./meta');
 const role = require('./role');
@@ -21,8 +19,6 @@ module.exports = function (app) {
   app.configure(role);
   app.configure(meta);
   app.configure(resetPassword);
-  app.configure(room);
   app.configure(roomReservation);
-  app.configure(building);
   app.configure(organization);
 };
