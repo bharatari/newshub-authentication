e4b18ccecec2e6a9ff632d7c5c55bab86d3b7842
Bharat Arimilli <bbarimilli@outlook.com>
2018-05-18 23:31:07 -0500

    Remove services from index

--- src/services/index.js
+++ src/services/index.js
@@ -3,21 +3,17 @@
 const organization = require('./organization');
 const roomReservation = require('./roomReservation');
 const resetPassword = require('./resetPassword');
-const meta = require('./meta');
 const role = require('./role');
 const image = require('./image');
 const signupToken = require('./signupToken');
-const notification = require('./notification');
 const user = require('./user');
 const Sequelize = require('sequelize');
 
 module.exports = function (app) {
   app.configure(user);
-  app.configure(notification);
   app.configure(signupToken);
   app.configure(image);
   app.configure(role);
-  app.configure(meta);
   app.configure(resetPassword);
   app.configure(roomReservation);
   app.configure(organization);
