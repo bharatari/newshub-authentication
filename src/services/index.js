11bae1c41c65306b5b50fccd02b7693e9ce15b22
Bharat Arimilli <bbarimilli@outlook.com>
2018-06-17 12:11:13 -0500

    Remove signup token service

--- src/services/index.js
+++ src/services/index.js
@@ -4,13 +4,11 @@ const organization = require('./organization');
 const resetPassword = require('./resetPassword');
 const role = require('./role');
 const image = require('./image');
-const signupToken = require('./signupToken');
 const user = require('./user');
 const Sequelize = require('sequelize');
 
 module.exports = function (app) {
   app.configure(user);
-  app.configure(signupToken);
   app.configure(image);
   app.configure(role);
   app.configure(resetPassword);
