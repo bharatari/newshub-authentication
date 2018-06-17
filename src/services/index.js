dfd85611a13fd983499dd7f703890f5022ac9090
Bharat Arimilli <bbarimilli@outlook.com>
2018-06-17 14:49:57 -0500

    Remove image service

--- src/services/index.js
+++ src/services/index.js
@@ -3,13 +3,11 @@
 const organization = require('./organization');
 const resetPassword = require('./resetPassword');
 const role = require('./role');
-const image = require('./image');
 const user = require('./user');
 const Sequelize = require('sequelize');
 
 module.exports = function (app) {
   app.configure(user);
-  app.configure(image);
   app.configure(role);
   app.configure(resetPassword);
   app.configure(organization);
